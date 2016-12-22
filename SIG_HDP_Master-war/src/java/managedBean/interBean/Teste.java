19305517f97d1db8b784ee8cebf665c0ffb5e312
armindo <armindo@armindo-SATELLITE-L755>
2016-12-22 18:26:20 +0100

    new commit

---
+++ SIG_HDP_Master-war/src/java/managedBean/interBean/Teste.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package managedBean.interBean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author armindo
+ */
+@ManagedBean
+@SessionScoped
+public class Teste
+{
+
+    /**
+     * Creates a new instance of Teste
+     */
+    public Teste()
+    {
+    }
+    
+}
